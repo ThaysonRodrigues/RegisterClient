2ec42ee281ae8dccb15aaa6e81492b11ca4f0a74
Thayson Rodrigues de Souza <contato.thayson@gmail.com>
2021-03-21 18:26:21 -0300

    modifying database for postgresql

--- src/main/java/br/com/api/entity/Client.java
+++ src/main/java/br/com/api/entity/Client.java
@@ -5,16 +5,20 @@ import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.Table;
+
+import org.hibernate.annotations.GenericGenerator;
 
 @Entity
+@Table(name = "clients")
 public class Client implements Serializable {
 
 	private static final long serialVersionUID = 1L;
 	
 	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@GeneratedValue(generator = "increment")
+	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	
 	@Column(length = 50, nullable = false)
